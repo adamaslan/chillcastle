8e6c8e15f09d1559323a2abab623eabdb0877123
Adam Aslan <chillcoders@gmail.com>
2024-01-26 18:59:34 -0500

    scrapey2

--- first4.js
+++ first4.js
@@ -8,18 +8,27 @@ async function scrape() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Go to the webpage you want to scrape
-  await page.goto('https://www.chillcastle.com/first4points');
+  // Define an array of URLs to scrape
+  const urls = [
+    'https://www.chillcastle.com/first4points',
+    'https://www.chillcastle.com/thenextpoints',
+    'https://www.chillcastle.com/42pointsofidealsandterrors',
+    'https://www.chillcastle.com/next42points',
+    'https://www.chillcastle.com/25points-woops'
+  ];
 
-  // Select the element that contains the text and get its innerText
-  const element = await page.$('.post-content');
-  const text = await element.evaluate(el => el.innerText);
+  // Loop over the URLs
+  for (let url of urls) {
+    // Go to the webpage you want to scrape
+    await page.goto(url);
 
-  // Convert the text to a JSON object
-  const data = JSON.parse(text);
+    // Select the element that contains the text and get its innerText
+    const body = await page.$('body');
+    const text = await body.evaluate(el => el.innerText);
 
-  // Write the data to a JSON file
-  fs.writeFileSync('data.json', JSON.stringify(data));
+    // Append the text to the data.json file
+    fs.appendFileSync('data2.json', text + '\n');
+  }
 
   // Close the browser
   await browser.close();
@@ -27,3 +36,37 @@ async function scrape() {
 
 // Call the function
 scrape();
+
+
+
+// // Import the modules
+// const puppeteer = require('puppeteer');
+// const fs = require('fs');
+
+// // Define an async function
+// async function scrape() {
+//   // Launch a browser and create a page
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+
+//   // Go to the webpage you want to scrape
+//   await page.goto('https://www.chillcastle.com/first4points');
+
+//   // Select the element that contains the text and get its innerText
+//   const body = await page.$('body');
+//   const text = await body.evaluate(el => el.innerText);
+
+//   // Convert the text to a JSON object
+//   // const data = JSON.parse(text);
+
+//   // Write the data to a JSON file
+//   // fs.writeFileSync('data.json', JSON.stringify(data));
+
+//   fs.writeFileSync('data.json', text);
+
+//   // Close the browser
+//   await browser.close();
+// }
+
+// // Call the function
+// scrape();
